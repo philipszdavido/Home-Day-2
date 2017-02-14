ab9939f86675abf63e44a5c3246ce459820ebe9c
philipszdavido <kurtwanger5@gmail.com>
2017-02-14 11:34:32 +0100

    Updated the words() and reverseString() function

--- wordCount/src/wordCount.js
+++ wordCount/src/wordCount.js
@@ -1,9 +1,14 @@
 'use strict';
 function words(string) {//defined my words function here.
+	//checks if an actual string object is passed.
+	if(typeof(string)=="string"){
 	var wordCounts = {};
 	var allWords = string.split(/\s+/);
 	for (var i = 0; i < allWords.length; i++) {
 		wordCounts[allWords[i]] = (!wordCounts.hasOwnProperty(allWords[i]) ? 1 : wordCounts[allWords[i]] + 1);
 	}
 	return wordCounts;
+}else{
+	return "please, input a string"
+}
 }
